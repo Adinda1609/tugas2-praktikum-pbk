032760c726b7adbb9d63f2c6f40a3e53a990520f
Sri Adinda <sriadinda@student.uir.ac.id>
2025-06-10 17:44:35 +0700

    Import font-awesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// === Font Awesome Setup ===
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Tambahkan ikon yang akan digunakan ke library
+library.add(faTrash)
+const app = createApp(App)
+
+// Daftarkan komponen font-awesome-icon secara global
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
