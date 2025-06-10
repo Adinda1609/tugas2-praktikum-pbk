c72b812d5efbb2c8be8f11c91cd707c53127d1a3
Sri Adinda <sriadinda@student.uir.ac.id>
2025-06-10 17:35:33 +0700

    Nampilin kegiatan yang belum dilakukan

--- src/App.vue
+++ src/App.vue
@@ -128,7 +128,33 @@ onMounted(() => {
             </div>
           </div>
         </div>
-      </div> 
-    </section>
+
+        <!-- Kegiatan yang belum dilakukan -->
+        <div class="todo-column">
+          <h4>Kegiatan yang belum dilakukan</h4>
+          <div class="list">
+            <div v-for="todo in todos_asc.filter(t => !t.done)" :key="todo.createdAt" :class="`todo-item ${todo.done && 'done'}`">
+              <label>
+                <!-- MEMAKAI FORM BINDINGS -->
+                <input type="checkbox" v-model="todo.done" />
+                <span :class="`bubble ${ todo.category == 'bisnis' ? 'bisnis' : 'personal'}`"></span>
+              </label>
+                <!-- MEMAKAI FORM BINDINGS -->
+              <div class="todo-content">
+                <input type="text" v-model="todo.content" />
+              </div>
+              <div class="actions">
+                <!-- MEMAKAI EVENT LISTENER -->
+                <button class="delete" @click="removeTodo(todo)">
+                  <font-awesome-icon :icon="['fas', 'trash']" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> 
+
+    
   </main>  
 </template>
